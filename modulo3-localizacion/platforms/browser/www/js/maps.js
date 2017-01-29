0197af58e827e9a1608373fe734b8e32d9e66985
Raúl Bojo <bojo.raul@gmail.com>
2017-01-29 20:03:49 +0100

    Módulo 3. Localización. Paso 2

--- modulo3-localizacion/platforms/browser/www/js/maps.js
+++ modulo3-localizacion/platforms/browser/www/js/maps.js
@@ -10,12 +10,18 @@ var app={
     },
 
     dispositivoListo: function() {
-        navigator.geolocation.getCurrentPosition(app.dibujaCoordenadas, app.errorAlSolicitarLocalizaacion);    
+        navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizaacion);    
     },
 
-    dibujaCoordenadas: function(position) {
-        var coordsDiv = document.querySelector('#coords');
-        coordsDiv.innerHTML = 'Latitud:' + position.coords.latitude + ' Longitud:' + position.coords.longitude;
+	pintaCoordenadasEnMapa: function(position) {
+		// 'L' hace referencia a un objeto de la librería leaflet.js
+		var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+
+		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmJyZGV2cyIsImEiOiJjaXlpejRtZHcwMDR5MnFueDQ2M29lOXB1In0.WXkJ-mpQo1MbbD4n9mvgGQ', {
+      		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		    maxZoom: 18
+    	}).addTo(miMapa);
+
     },
     
     errorAlSolicitarLocalizaacion: function(error) {
