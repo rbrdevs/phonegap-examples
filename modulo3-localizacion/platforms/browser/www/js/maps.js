b149a7a5fa3c7cdbae759eaf6e7ac21729961f59
Raúl Bojo <bojo.raul@gmail.com>
2017-01-31 23:35:00 +0100

    Módulo 3. Localización. Práctica - marcadores y seguimiento GPS

--- modulo3-localizacion/platforms/browser/www/js/maps.js
+++ modulo3-localizacion/platforms/browser/www/js/maps.js
@@ -9,13 +9,30 @@
  */
 
 var app={
+    watchPosition : undefined,
+    miMapa: undefined,
+    marcador: undefined,
+    circle: undefined,
     inicio: function() {
-        this.iniciaFastClick();        
+        this.iniciaFastClick(); 
+        this.iniciaBotones();       
     },
 
     iniciaFastClick: function() {
         FastClick.attach(document.body);
     },
+
+    iniciaBotones: function() {
+        var botonStart = document.querySelector('#button-start');
+        botonStart.addEventListener('click', function() {
+            app.watchPosition = app.startWatchMapPosition();
+        });
+        var botonStop = document.querySelector('#button-stop');
+        botonStop.addEventListener('click', function() {
+            app.stopWatchMapPosition();
+        });
+    },
+
 
     dispositivoListo: function() {
         navigator.geolocation.getCurrentPosition(app.pintaCoordenadasEnMapa, app.errorAlSolicitarLocalizaacion);    
@@ -23,18 +40,40 @@ var app={
 
 	pintaCoordenadasEnMapa: function(position) {
 		// 'L' hace referencia a un objeto de la librería leaflet.js
-		var miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+		app.miMapa = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
 		L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmJyZGV2cyIsImEiOiJjaXlpejRtZHcwMDR5MnFueDQ2M29lOXB1In0.WXkJ-mpQo1MbbD4n9mvgGQ', {
       		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		    maxZoom: 18
-    	}).addTo(miMapa);
+    	}).addTo(app.miMapa);
+ 
 
-        app.pintaMarcador([position.coords.latitude,position.coords.longitude],'¡Estoy aquí!', miMapa);
+        var greenIcon = L.icon({
+            iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+            shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
 
-        miMapa.on('click', function(evento) {
+            iconSize:     [38, 95], // size of the icon
+            shadowSize:   [50, 64], // size of the shadow
+            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        //app.pintaMarcador([position.coords.latitude,position.coords.longitude],'¡Estoy aquí!', miMapa);
+        app.marcador = L.marker([position.coords.latitude,position.coords.longitude], {icon: greenIcon}).addTo(app.miMapa);
+        app.marcador.bindPopup('¡Estoy aquí!').openPopup();
+        
+
+        app.circle = L.circle([position.coords.latitude, position.coords.longitude], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 1000
+        }).addTo(app.miMapa);
+
+        app.miMapa.on('click', function(evento) {
             var texto = 'Marcador en l('+evento.latlng.lat.toFixed(2)+') y L('+evento.latlng.lng.toFixed(2)+')';
-            app.pintaMarcador(evento.latlng,texto,miMapa);
+            app.pintaMarcador(evento.latlng,texto,app.miMapa);
         });
     },
     
@@ -45,7 +84,32 @@ var app={
 
     errorAlSolicitarLocalizaacion: function(error) {
         console.log(error.code + ': '+error.message);
-    }
+    },
+
+
+    onMapWatchSuccess: function (position) {
+        console.log(position.coords.latitude+' - '+position.coords.longitude);
+        var updatedLatitude = position.coords.latitude;
+        var updatedLongitude = position.coords.longitude;
+
+        //if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+        app.miMapa.setView([updatedLatitude, updatedLongitude]);
+        app.marcador.setLatLng([updatedLatitude, updatedLongitude]);
+        app.circle.setLatLng([updatedLatitude, updatedLongitude]);
+        //}
+    },
+
+    // Watch your changing position
+
+    startWatchMapPosition: function() {
+        console.log("start");
+        return navigator.geolocation.watchPosition
+        (app.onMapWatchSuccess, app.errorAlSolicitarLocalizaacion, { enableHighAccuracy: true });
+    },
+    stopWatchMapPosition: function() {
+        console.log("stop");
+        navigator.geolocation.clearWatch(app.watchPosition);
+    },        
 };
 
 if ('addEventListener' in document) {
