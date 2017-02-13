324935cad93fba30fa4ee77415132c3efe37794d
Raúl Bojo <bojo.raul@gmail.com>
2017-02-13 23:44:48 +0100

    Módulo 4. Sensores - Paso1 - ok

--- modulo4-sensores/www/js/juego.js
+++ modulo4-sensores/www/js/juego.js
@@ -15,11 +15,12 @@ var app={
         navigator.accelerometer.watchAcceleration(this.onSuccess, onError,{ frequency: 1000});
     },
     onSuccess: function(datosAceleracion) {
+        console.log("onSuccess");
         app.representa(datosAceleracion.x,'#valorx');
         app.representa(datosAceleracion.y,'#valory');
         app.representa(datosAceleracion.z,'#valorz');
     },
-    representa: function(sAceleracion) {
+    representa: function(dato, elementoHTML) {
         redondeo = Math.round(dato * 100) / 100;
         document.querySelector(elementoHTML).innerHTML = redondeo;
     }
